15ac158a654d09f3fc71d4a0362129dec1c5ebcf
mohhamednabil <mohammednabil@hungerstation.com>
2018-12-16 11:50:13 +0300

    Revert "connection_fix"

--- app/src/main/java/com/usepace/android/messagingcenter/clients/connection_client/SendBirdClient.java
+++ app/src/main/java/com/usepace/android/messagingcenter/clients/connection_client/SendBirdClient.java
@@ -53,13 +53,6 @@ class SendBirdClient extends ClientInterface {
                                             connectionInterface.onMessageCenterConnectionError(e.getCode(), new MessageCenterException(e.getMessage()));
                                         }
                                         else {
-                                            //Disconnecting for receiving push notifications
-                                            SendBird.disconnect(new SendBird.DisconnectHandler() {
-                                                @Override
-                                                public void onDisconnected() {
-
-                                                }
-                                            });
                                             connectionInterface.onMessageCenterConnected();
                                         }
                                     }
