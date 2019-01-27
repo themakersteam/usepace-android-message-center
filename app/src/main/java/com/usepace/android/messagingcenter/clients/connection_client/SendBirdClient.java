d00629bf81d5daa9de162e2c9fe006d70aaf3cb3
Samy Baili <samy.baili@gmail.com>
2019-01-27 11:39:18 +0300

    Fix Runtime exception

--- app/src/main/java/com/usepace/android/messagingcenter/clients/connection_client/SendBirdClient.java
+++ app/src/main/java/com/usepace/android/messagingcenter/clients/connection_client/SendBirdClient.java
@@ -46,7 +46,6 @@ class SendBirdClient extends ClientInterface {
             mainConnectCalled = true;
             this.lastConnecitonRequest = connectionRequest;
             SendBird.init(connectionRequest.getAppId(), context);
-            SendBird.setAutoBackgroundDetection(true);
             SendBird.connect(connectionRequest.getUserId() != null ? connectionRequest.getUserId() : "", connectionRequest.getAccessToken(), new SendBird.ConnectHandler() {
                 @Override
                 public void onConnected(User user, final SendBirdException e) {
